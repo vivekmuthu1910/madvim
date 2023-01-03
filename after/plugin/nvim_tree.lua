f88ba68105fbe527ad6d1a14538937298f9b2906
Vivekanandan Muthaiyan <vivekmuthu1910@gmail.com>
2023-01-03 07:55:34 +0530

    added nvim tree

---
+++ after/plugin/nvim_tree.lua
@@ -0,0 +1,30 @@
+-- examples for your init.lua
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<C-h>f", ":NvimTreeFindFile<cr>", { silent = true, noremap = true })
+
+-- empty setup using defaults
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
